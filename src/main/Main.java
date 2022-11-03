cecc1ee43c23a263b5bbcfaf264c9d77f98f74f9
jcaicedor <jul.cruizz@gmail.com>
2022-11-03 10:28:22 -0500

    Migrate from Log4J to Log4J2

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,18 @@
 package main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+   private static final Logger LOGGER = LogManager.getLogger();
    public static void main(String[] args) {
+      /*TODO:
+      *  1. Create interface  -DONE
+      *  2. Migrate from Log4J to Log4J2  -DONE
+      *  3. Implement interface in Dentist and Patient DAO
+      *  4. Create service classes*/
+
+      LOGGER.info("First log. Main class running.");
 
    }
 }
