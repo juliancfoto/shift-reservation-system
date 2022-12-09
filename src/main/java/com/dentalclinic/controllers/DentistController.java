e199b9950eaeecc87226aab0fb00bd75e7012a1b
jcaicedor <jul.cruizz@gmail.com>
2022-12-08 21:35:59 -0500

    Using ResponseEntity in controller classes

--- src/main/java/com/dentalclinic/controllers/DentistController.java
+++ src/main/java/com/dentalclinic/controllers/DentistController.java
@@ -4,6 +4,7 @@ import com.dentalclinic.dto.DentistDTO;
 import com.dentalclinic.entities.Dentist;
 import com.dentalclinic.services.DentistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,29 +22,38 @@ public class DentistController {
 
    // CREATE A DENTIST
    @PostMapping("/new")
-   public Dentist create(@RequestBody Dentist dentist) {
-      return dentistService.createDentist(dentist);
+   public ResponseEntity<Dentist> create(@RequestBody Dentist dentist) {
+      return ResponseEntity.ok().body(dentistService.createDentist(dentist));
    }
 
    // SEARCH ALL DENTISTS
    @GetMapping("/search")
-   public List<Dentist> searchAll() {
-      return dentistService.readAllDentists();
+   public ResponseEntity<List<DentistDTO>> searchAll() {
+      return ResponseEntity.ok().body(dentistService.readAllDentists());
    }
 
    // SEARCH A DENTIST BY ID
    @GetMapping("/search/{id}")
-   public DentistDTO searchById(@PathVariable Long id) {
-      return dentistService.readDentist(id);
+   public ResponseEntity<DentistDTO> searchById(@PathVariable Long id) {
+      return ResponseEntity.ok().body(dentistService.readDentist(id));
    }
 
    // UPDATE A DENTIST
    @PutMapping("/update")
-   public Dentist update(@RequestBody Dentist dentist) {
-      return dentistService.updateDentist(dentist);
+   public ResponseEntity<Dentist> update(@RequestBody Dentist dentist) {
+      /*ResponseEntity<Dentist> response;
+
+      if (dentistService.readDentist(dentist.getId()) == null) {
+         response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      } else {
+         response = new ResponseEntity<>(dentistService.updateDentist(dentist), HttpStatus.OK);
+      }
+
+      return response;*/
+      return ResponseEntity.ok().body(dentistService.updateDentist(dentist));
    }
 
-   // DELETE A DENTIST
+   // DELETE A DENTIST TODO RESPONSE ENTITY
    @DeleteMapping("/delete/{id}")
    public void delete(@PathVariable Long id) {
       dentistService.deleteDentist(id);
--- src/main/java/com/dentalclinic/controllers/PatientController.java
+++ src/main/java/com/dentalclinic/controllers/PatientController.java
@@ -4,6 +4,7 @@ import com.dentalclinic.dto.PatientDTO;
 import com.dentalclinic.entities.Patient;
 import com.dentalclinic.services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,29 +22,29 @@ public class PatientController {
 
    // CREATE A PATIENT
    @PostMapping("/new")
-   public Patient create(@RequestBody Patient patient) {
-      return patientService.createPatient(patient);
+   public ResponseEntity<Patient> create(@RequestBody Patient patient) {
+      return ResponseEntity.ok().body(patientService.createPatient(patient));
    }
 
    // SEARCH ALL PATIENTS
    @GetMapping("/search")
-   public List<Patient> searchAll() {
-      return patientService.readAllPatients();
+   public ResponseEntity<List<PatientDTO>> searchAll() {
+      return ResponseEntity.ok().body(patientService.readAllPatients());
    }
 
    // SEARCH A PATIENT BY ID USING DTO
    @GetMapping("/search/{id}")
-   public PatientDTO searchById(@PathVariable("id") Long id) {
-      return patientService.readPatient(id);
+   public ResponseEntity<PatientDTO> searchById(@PathVariable("id") Long id) {
+      return ResponseEntity.ok().body(patientService.readPatient(id));
    }
 
    // UPDATE A PATIENT
    @PutMapping("/update")
-   public Patient update(@RequestBody Patient patient) {
-      return patientService.updatePatient(patient);
+   public ResponseEntity<Patient> update(@RequestBody Patient patient) {
+      return ResponseEntity.ok().body(patientService.updatePatient(patient));
    }
 
-   // DELETE A PATIENT
+   // DELETE A PATIENT TODO RESPONSE ENTITY
    @DeleteMapping("/delete/{id}")
    public void delete(@PathVariable Long id) {
       patientService.deletePatient(id);
